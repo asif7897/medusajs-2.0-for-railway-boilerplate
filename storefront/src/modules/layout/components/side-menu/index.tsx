4a95442becc5764a0ce6d135f47e3ba5f855bad0
asif7897 <asifmahmud7894@gmail.com>
2024-11-26 05:51:21 +0600

    sidemenu_pant

--- storefront/src/modules/layout/components/side-menu/index.tsx
+++ storefront/src/modules/layout/components/side-menu/index.tsx
@@ -3,15 +3,21 @@
 import { Popover, Transition } from "@headlessui/react"
 import { ArrowRightMini, XMark } from "@medusajs/icons"
 import { Text, clx, useToggleState } from "@medusajs/ui"
-import { Fragment } from "react"
+import { Fragment ,useState } from "react"
 
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CountrySelect from "../country-select"
 import { HttpTypes } from "@medusajs/types"
 
+import { Arrow, Arrow2 } from '@modules/layout/components/Icons/index'
+
+
+
+
 const SideMenuItems = {
   Home: "/",
   Store: "/store",
+  "Shop By Category": "/",
   Search: "/search",
   Account: "/account",
   Cart: "/cart",
@@ -19,6 +25,62 @@ const SideMenuItems = {
 
 const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
   const toggleState = useToggleState()
+  const [showCategory, setShowCategory] = useState(false)
+
+  const LinkText = "text-xl leading-10 hover:text-ui-fg-disabled poppins flex items-center text-[#fff] justify-between"
+  const mainBox = "flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6 min-w-fit w-full max-w-[350px]"
+  const [trigger, setTrigger] = useState(0);
+
+  const handleClick = () => {
+    setTrigger(prev => prev + 1); // Increment trigger to re-run animation
+  };
+
+  const Arr = [
+    {
+      "main_category": "Men",
+      "sub_categories": [
+      //  { "name": "Blazer", "handle": "blazer" },
+      //  { "name": "Single Blazer", "handle": "s_blazer" },
+      //  { "name": "Box-tie", "handle": "box_tie" },
+      //  { "name": "Suit", "handle": "suit" },
+      //  { "name": "Punjabi", "handle": "punjabi" },
+      //  { "name": "Formal Shirt", "handle": "shirt" },
+      //  { "name": "Pant", "handle": "pant" },
+
+        //new add 3 aug 2024  
+
+        { "name": "Premium Suit", "handle": "premium_suit" },
+        { "name": "Platinum Blazer", "handle": "platinum_blazer" },
+        { "name": "Formal Shirt", "handle": "formal_shirt" },
+       // { "name": "Elite Panjabi", "handle": "elite_panjabi" },
+        { "name": "Half Coat", "handle": "half_coat" },
+        { "name": "Formal Pant", "handle": "pants" } //formal pants ok .. 
+
+
+
+      ],
+      image: ["https://medusajs-frontend-production-28be.up.railway.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnf2535.9d8b5746.png&w=384&q=75", "https://medusajs-frontend-production-28be.up.railway.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fst1366.ea9d30dc.png&w=384&q=75"]
+    },
+    {
+      "main_category": "Women",
+      "sub_categories": [
+       // { "name": "Blazer", "handle": "blazer_w" },
+      //  { "name": "Single Blazer", "handle": "s_blazer_w" },
+        { "name": "Women's-suit", "handle": "women's_suit" }
+      ],
+      image: ["https://medusajs-frontend-production-28be.up.railway.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fwomen-1.dc76cb6f.jpg&w=384&q=75", "https://medusajs-frontend-production-28be.up.railway.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fwomen-2.031fff47.jpg&w=384&q=75"]
+    },
+    {
+      "main_category": "Accessory",
+      "sub_categories": [
+        { "name": "Box-tie", "handle": "box_tie" },
+        { "name": "Luxury-belt", "handle": "luxury_belt" },
+        //3 aug 
+      //  { "name": "Tie-pin", "handle": "tie_pin" }
+      ],
+      image: ['https://medusajs-frontend-production-28be.up.railway.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Facces-1.69702ddf.jpg&w=384&q=75', "https://medusajs-frontend-production-28be.up.railway.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Facces-2.f358fb03.jpg&w=384&q=75"]
+    }
+  ]
 
   return (
     <div className="h-full">
@@ -27,10 +89,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
           {({ open, close }) => (
             <>
               <div className="relative flex h-full">
-                <Popover.Button
-                  data-testid="nav-menu-button"
-                  className="relative h-full flex items-center transition-all ease-out duration-200 focus:outline-none hover:text-ui-fg-base"
-                >
+                <Popover.Button className="poppins relative h-full flex items-center transition-all ease-out duration-200 focus:outline-none hover:text-white">
                   Menu
                 </Popover.Button>
               </div>
@@ -45,57 +104,122 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                 leaveFrom="opacity-100 backdrop-blur-2xl"
                 leaveTo="opacity-0"
               >
-                <Popover.Panel className="flex flex-col absolute w-full pr-4 sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-30 inset-x-0 text-sm text-ui-fg-on-color m-2 backdrop-blur-2xl">
-                  <div
-                    data-testid="nav-menu-popup"
-                    className="flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6"
-                  >
-                    <div className="flex justify-end" id="xmark">
-                      <button data-testid="close-menu-button" onClick={close}>
-                        <XMark />
-                      </button>
-                    </div>
-                    <ul className="flex flex-col gap-6 items-start justify-start">
-                      {Object.entries(SideMenuItems).map(([name, href]) => {
-                        return (
-                          <li key={name}>
-                            <LocalizedClientLink
-                              href={href}
-                              className="text-3xl leading-10 hover:text-ui-fg-disabled"
-                              onClick={close}
-                              data-testid={`${name.toLowerCase()}-link`}
-                            >
-                              {name}
-                            </LocalizedClientLink>
-                          </li>
-                        )
-                      })}
-                    </ul>
-                    <div className="flex flex-col gap-y-6">
-                      <div
-                        className="flex justify-between"
-                        onMouseEnter={toggleState.open}
-                        onMouseLeave={toggleState.close}
-                      >
-                        {regions && (
-                          <CountrySelect
-                            toggleState={toggleState}
-                            regions={regions}
-                          />
-                        )}
-                        <ArrowRightMini
-                          className={clx(
-                            "transition-transform duration-150",
-                            toggleState.state ? "-rotate-90" : ""
-                          )}
-                        />
+                <Popover.Panel className="flex flex-col absolute w-full  sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-30 inset-x-0 text-sm text-ui-fg-on-color m-2 backdrop-blur-2xl pr-0"
+                  style={{
+                    // minWidth: "fit-content",
+                    width: "100%",
+                    maxWidth: "350px"
+                  }}
+                >
+
+                  {showCategory ? <>
+                    <div className={mainBox} style={{
+                      // minWidth: "fit-content",
+                      width: "100%",
+                      maxWidth: "350px"
+                    }}>
+                      <div className="flex flex-col min-w-fit w-full max-w-[350px]">
+                        <div className="flex justify-start gap-[10px]" id="xmark">
+                          <button onClick={() => {
+                            setShowCategory(false)
+                            handleClick()
+                          }}>
+                            <Arrow2 size="30px" color="#fff" />
+                          </button>
+                          <p className={'text-2xl leading-10 text-[#fff] poppins'}>Shop by category</p>
+                        </div>
+                        <div className="flex flex-col">
+                          {Arr.map((menu, index) => (
+                            <div key={index} className={` flex flex-col poppins mt-[15px]`}>
+                              <h3 className='poppins text-[15px] font-[600] font-[#000] mb-[10px]'>{menu.main_category}</h3>
+                              {menu.sub_categories && menu.sub_categories.map((subMenu, subIndex) => (
+                                <div key={subIndex} className={''}>
+                                  <ul>
+                                    <li><LocalizedClientLink
+                                      href={`/categories/${subMenu.handle}`}
+                                      className='poppins font-[300] tracking-[0.6px]'>{subMenu.name}</LocalizedClientLink></li>
+                                  </ul>
+                                </div>
+                              ))}
+                            </div>
+                          ))}
+                        </div>
                       </div>
-                      <Text className="flex justify-between txt-compact-small">
-                        © {new Date().getFullYear()} Medusa Store. All rights
-                        reserved.
-                      </Text>
                     </div>
-                  </div>
+                  </> :
+                    <div style={{
+                      // minWidth: "fit-content",
+                      width: "100%",
+                      maxWidth: "350px"
+                    }} className="h-full">
+                      <div className={mainBox} style={{
+                        minWidth: "0%"
+                      }}>
+                        <div className="flex justify-end" id="xmark">
+                          <button onClick={close}>
+                            <XMark />
+                          </button>
+                        </div>
+                        <ul className="flex flex-col gap-6 items-start justify-start">
+                          {Object.entries(SideMenuItems).map(([name, href], index) => {
+                            if (name == "Shop By Category") {
+                              return (
+                                <li key={name} className="w-full sm:hidden xs: block ">
+                                  <LocalizedClientLink
+                                    href={href}
+                                    className={LinkText}
+                                    onClick={() => {
+                                      handleClick()
+                                      setShowCategory(true)
+                                    }}
+                                  >
+                                    {name} <div><Arrow size="30px" color="#fff" /></div>
+                                  </LocalizedClientLink>
+                                </li>)
+                            } else {
+                              return (
+                                <li key={name}>
+                                  <LocalizedClientLink
+                                    href={href}
+                                    className={LinkText}
+                                    onClick={close}
+                                  >
+                                    {name}
+                                  </LocalizedClientLink>
+                                </li>
+                              )
+                            }
+
+                          })}
+                        </ul>
+                        <div className="flex flex-col gap-y-6">
+                          <div
+                            className="flex justify-between"
+                            onMouseEnter={toggleState.open}
+                            onMouseLeave={toggleState.close}
+                          >
+                            {regions && (
+                              <CountrySelect
+                                toggleState={toggleState}
+                                regions={regions}
+                              />
+                            )}
+                            <ArrowRightMini
+                              className={clx(
+                                "transition-transform duration-150",
+                                toggleState.state ? "-rotate-90" : ""
+                              )}
+                            />
+                          </div>
+                          <Text className="flex justify-between txt-compact-small">
+                            © {new Date().getFullYear()} Sea Sky. All rights
+                            reserved.
+                          </Text>
+                        </div>
+                      </div>
+                    </div>
+                  }
+
                 </Popover.Panel>
               </Transition>
             </>
