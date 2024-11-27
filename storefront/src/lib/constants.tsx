f3681039096407b763ade91214818bb642726073
asif7897 <asifmahmud7894@gmail.com>
2024-11-27 10:45:31 +0600

    cash on delivary

--- storefront/src/lib/constants.tsx
+++ storefront/src/lib/constants.tsx
@@ -27,7 +27,7 @@ export const paymentInfoMap: Record<
     icon: <PayPal />,
   },
   pp_system_default: {
-    title: "Manual Payment",
+    title: "Cash On Delivary",
     icon: <CreditCard />,
   },
   // Add more payment providers here
