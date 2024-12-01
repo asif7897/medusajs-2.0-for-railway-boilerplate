9535d040e191f876edfcd1b4d225151a69f450d3
asif7897 <asifmahmud7894@gmail.com>
2024-12-01 12:09:54 +0600

    women section

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,39 +1,39 @@
-import { Github } from "@medusajs/icons";
-import { Button, Heading } from "@medusajs/ui";
+"use client"; // Indicate this is a Client Component
 
-const Hero = () => {
+import React from "react";
+import Link from "next/link";
+
+const Hero: React.FC = () => {
   const backgroundImageUrl =
     "https://res.cloudinary.com/dvzdodv1r/image/upload/v1732105664/woment-min_pk6isx_b17qrz.webp";
 
   return (
     <div
-      className="h-[75vh] w-full border-b border-ui-border-base relative bg-cover bg-center"
+      className="hero-wrapper relative overflow-hidden rounded-lg"
       style={{
         backgroundImage: `url(${backgroundImageUrl})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        minHeight: "75vh", // Match height for consistent styling
       }}
     >
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Well done! You have successfully deployed your Medusa 2.0 store on
-            Railway!
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Need help customizing your store?
-          </Heading>
-        </span>
-        <a
-          href="https://funkyton.com/medusajs-2-0-is-finally-here/"
-          target="_blank"
-        >
-          <h1 style={{ textDecoration: "underline" }}>Visit the tutorial</h1>
-        </a>
+      {/* Overlay */}
+      <div className="absolute inset-0 h-full w-full flex items-center justify-center">
+        <div className="text-white text-center px-4 md:px-8">
+           
+ 
+
+          {/* Shop Now Button */}
+          <Link href="/collections/womenSuit">
+            <button
+              className="rounded border-2 border-neutral-50 px-7 py-3 text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:outline-none"
+            >
+              Shop Now
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
